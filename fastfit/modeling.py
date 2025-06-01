fix normalize scores formula

fastfit/modeling.py
@@ -782,9 +782,8 @@ class FastFitTrainable(PreTrainedModel):
             lens = tokens_mask.max(2)[0].sum(2)
             tokens_sim = tokens_sim * tokens_mask
 
-        scores = tokens_sim.max(2)[0].sum(2)
-        scores = scores / scores.mean()
-        scores = torch.clamp(scores, 0, 1)
+        max_value = tokens_mask.max(-1)[0].unsueeze(-1)
+        scores = scores / max_value
 
         """if with_lens_norm:
             scores = scores / lens